7b10eb4f64a49956b9961b776d7ab70757447cfa
Marto Nieto Guerrero <marto.nieto.g16@gmail.com>
2023-05-22 21:31:47 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,60 +1,42 @@
-# app.py
-from flask import Flask, render_template, request, jsonify
-from coinbase.wallet.client import Client
-import redis
-import time
+from flask import Flask, request
+from web3 import Web3
 
 app = Flask(__name__)
-redis_db = redis.Redis()
+web3 = Web3(Web3.HTTPProvider('https://goerli.infura.io/v3/a4693816802c4b01bedef15ad5a2fd92'))
+private_key = 'a4693816802c4b01bedef15ad5a2fd92'
+address = '0xdbD9cFe329Fcb90529579Fe25282089464616CD0'
 
-API_KEY = 'uBT2LzeEiRZ0iMn7'
-API_SECRET = 'lUlwoziJzYqpSo9jS8tf5IStJpZ0Fdvq'
-client = Client(API_KEY, API_SECRET)
+def send_ether(to_address, amount):
+    nonce = web3.eth.getTransactionCount(address)
+    gas_price = web3.eth.gasPrice
+    value = web3.toWei(amount, 'ether')
 
-FAUCET_AMOUNT = 0.011  # Cantidad de criptomonedas que se distribuirán en cada reclamo
-CLAIM_INTERVAL = 42 * 60 * 60  # Intervalo de tiempo en segundos entre reclamos (42 horas)
+    transaction = {
+        'nonce': nonce,
+        'to': to_address,
+        'value': value,
+        'gas': 21000,
+        'gasPrice': gas_price,
+    }
 
-# Configura la conexión a Redis
-redis_host = 'redis://red-chlotgu7avj217fvm180'
-redis_port = 6379
-redis_db = 0
-redis_client = redis.Redis(host=redis_host, port=redis_port, db=redis_db)
+    signed_txn = web3.eth.account.signTransaction(transaction, private_key)
+    tx_hash = web3.eth.sendRawTransaction(signed_txn.rawTransaction)
+    receipt = web3.eth.waitForTransactionReceipt(tx_hash)
+    return receipt
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+@app.route('/faucet', methods=['POST'])
+def faucet():
+    to_address = request.form.get('address')
+    amount = request.form.get('amount')
 
-@app.route('/sponsorship')
-def sponsorship():
-    return render_template('sponsorship.html')
-
-@app.route('/about')
-def about():
-    return render_template('about.html')
-
-@app.route('/claim', methods=['POST'])
-def claim():
-    user_address = request.form.get('address')
-    last_claim_time = redis_db.get(user_address)
-
-    if last_claim_time:
-        time_since_last_claim = time.time() - float(last_claim_time)
-        if time_since_last_claim < CLAIM_INTERVAL:
-            return jsonify({'message': f'Puedes reclamar nuevamente en {CLAIM_INTERVAL - time_since_last_claim:.0f} segundos.'})
-
-    try:
-        # Enviamos la criptomoneda utilizando la API de Coinbase
-        response = client.send_money(user_address, FAUCET_AMOUNT, 'SHIB', description='Reclamo de Faucet')
-
-        # Verificamos si la transacción se realizó con éxito
-        if response and response.status == 'completed':
-            # Registramos el tiempo del último reclamo exitoso
-            redis_db.set(user_address, time.time())
-            return jsonify({'message': f'Reclamaste {FAUCET_AMOUNT} SHIB exitosamente.'})
-        else:
-            return jsonify({'message': 'Error al enviar la criptomoneda.'})
-    except Exception as e:
-        return jsonify({'message': f'Error: {str(e)}'})
+    if to_address and amount:
+        try:
+            send_ether(to_address, float(amount))
+            return 'Ether sent successfully!'
+        except Exception as e:
+            return str(e)
+    else:
+        return 'Invalid request'
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
