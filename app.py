fb688434979a108c3317a9dda284ecd8a8e0d767
Marto Nieto Guerrero <marto.nieto.g16@gmail.com>
2023-07-04 00:26:46 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,54 +1,36 @@
-from flask import Flask, render_template
-import sqlite3
-import threading
+import redis
 
-app = Flask(__name__)
-contador = 86400  # 24 horas en segundos
-contador_activo = False  # Indica si la cuenta regresiva está activa
-lock = threading.Lock()  # Bloqueo para asegurar operaciones atómicas
-
-def decrementar_contador():
-    global contador, contador_activo
-
-    while contador > 0:
-        with lock:
-            if not contador_activo:
-                break
-
-            contador -= 1
-
-        # Esperar 1 segundo
-        threading.Event().wait(1)
-
-    contador_activo = False
+# Configurar la conexión a Redis
+redis_client = redis.Redis(host='redis://red-cihqq559aq012evkgnkg', port=6379, db=0)
 
 @app.route('/')
 def index():
-    # Conectar a la base de datos
-    conn = sqlite3.connect('database.db')
-    cursor = conn.cursor()
-
-    # Obtener el valor actual del contador
-    cursor.execute('SELECT valor FROM contador WHERE id = 1')
-    contador = cursor.fetchone()[0]
-
-    # Cerrar la conexión
-    conn.close()
+    # Obtener el valor actual del contador desde Redis
+    contador = redis_client.get('contador')
+    if contador is None:
+        contador = 86400  # Valor predeterminado de 24 horas en segundos
+    else:
+        contador = int(contador)
 
     # Renderizar el template con el contador
     return render_template('index.html', contador=contador)
 
 @app.route('/decrementar')
 def decrementar():
-    global contador, contador_activo
+    # Obtener el valor actual del contador desde Redis
+    contador = redis_client.get('contador')
+    if contador is None:
+        contador = 86400  # Valor predeterminado de 24 horas en segundos
+    else:
+        contador = int(contador)
 
-    # Iniciar la cuenta regresiva si no está activa
-    if not contador_activo:
-        contador_activo = True
-        threading.Thread(target=decrementar_contador).start()
+    # Decrementar el contador en 1
+    contador -= 1
+
+    # Actualizar el contador en Redis
+    redis_client.set('contador', contador)
 
     # Renderizar el template con el contador actualizado
     return render_template('index.html', contador=contador)
 
-if __name__ == '__main__':
-    app.run()
+
